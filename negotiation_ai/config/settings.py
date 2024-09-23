fc546150a9d05d047d2b88af9837d53c61fe182a
Aryan Chavan <thearyanchavan30@gmail.com>
2024-09-23 12:40:09 +0530

    New Model Added

--- negotiation_ai/config/settings.py
+++ negotiation_ai/config/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-CHAT_MODEL = "llama3-70b-8192"
-MATH_MODEL = "llama3-70b-8192"
+CHAT_MODEL = "llama-3.1-70b-versatile"
+MATH_MODEL = "llama-3.1-70b-versatile"
 
 MAX_PRICE_INCREASE_RATIO = 1.35
